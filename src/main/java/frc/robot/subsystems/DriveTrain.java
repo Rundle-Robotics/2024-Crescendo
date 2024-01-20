4a3ab545b20826d1f800ec7de8c84460ef2dbf3c
DevenVElani <118859678+DevenVElani@users.noreply.github.com>
2024-01-19 18:20:35 -0700

    new branch with climber code
    just new file and basic code

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.OperatorConstants;
+import com.revrobotics.RelativeEncoder;
+public class DriveTrain {
+    public DriveTrain() {
+         
+    }
+    
+}
