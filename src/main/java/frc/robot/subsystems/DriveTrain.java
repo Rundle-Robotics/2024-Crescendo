5bbc5accfadccc0322d4229d15ed8fd9bb9f7929
DevenVElani <118859678+DevenVElani@users.noreply.github.com>
2024-01-23 12:43:13 -0700

    added encoder basics
    encoders functions now give basic values

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -28,16 +28,30 @@ public class DriveTrain extends SubsystemBase {
     private double backRight;
     private RelativeEncoder frontLefte;
     private RelativeEncoder frontRighte;
-    private RelativeEncoder backLefe;
+    private RelativeEncoder backLefte;
     private RelativeEncoder backRighte;
     private double x;
     private double y;
+    private double vel;
+    private double pos;
+    private double cpr;
     public DriveTrain() {
         FLmotor = new CANSparkMax(1, MotorType.kBrushless);
         FRmotor = new CANSparkMax(2, MotorType.kBrushless);
         BLmotor = new CANSparkMax(3, MotorType.kBrushless);
         BRmotor = new CANSparkMax(4, MotorType.kBrushless);
-
+        frontLefte = FLmotor.getEncoder();
+		frontRighte = FRmotor.getEncoder();
+		backLefte = BLmotor.getEncoder();
+		backRighte = BRmotor.getEncoder();
+        FLmotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+		FRmotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+		BLmotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+		BRmotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        FLmotor.setOpenLoopRampRate(1);
+		BLmotor.setOpenLoopRampRate(1);
+		FRmotor.setOpenLoopRampRate(1);
+		BRmotor.setOpenLoopRampRate(1);
     }
     @Override
     public void periodic() {
@@ -66,12 +80,61 @@ public class DriveTrain extends SubsystemBase {
             frontRight /= powerInside + turnInside;
             backRight /= powerInside + turnInside;
         }
+        SmartDashboard.putNumber("frontRight", frontRight);
+		SmartDashboard.putNumber("frontLeft", frontLeft);
+		SmartDashboard.putNumber("backLeft", backLeft);
+		SmartDashboard.putNumber("backRight", backLeft);
         // set speed to motors
         FLmotor.set(frontLeft);
         BLmotor.set(backLeft);
         FRmotor.set(frontRight);
         BRmotor.set(backRight);
 
+    }
+    public double getMotorVelocity(double motorNumber) {
+        if(motorNumber == 1) {
+            vel = frontLefte.getVelocity();
+        }
+        else if(motorNumber == 2) {
+            vel = backLefte.getVelocity();
+        }
+        else if(motorNumber == 3) {
+            vel = frontRighte.getVelocity();
+        }
+        else if(motorNumber == 4) {
+            vel = backRighte.getVelocity();
+        }
+        return vel;
+    }
+    public double getMotorPosition(double motorNumber) {
+        if(motorNumber == 1) {
+            pos = frontLefte.getPosition();
+        }
+        else if(motorNumber == 2) {
+            pos = backLefte.getPosition();
+        }
+        else if(motorNumber == 3) {
+            pos = frontRighte.getPosition();
+        }
+        else if(motorNumber == 4) {
+            pos = backRighte.getPosition();
+        }
+        return pos;
+    }
+    public double getMotorCountsPerRevolution(double motorNumber) {
+        if(motorNumber == 1) {
+            cpr = frontLefte.getCountsPerRevolution();
+        }
+        else if(motorNumber == 2) {
+            cpr = backLefte.getCountsPerRevolution();
+        }
+        else if(motorNumber == 3) {
+            cpr = frontRighte.getCountsPerRevolution();
+        }
+        else if(motorNumber == 4) {
+            cpr = backRighte.getCountsPerRevolution();
+        }
+        return cpr;
     }
     
 }
