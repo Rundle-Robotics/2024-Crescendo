6d00f632a6b2f7f0d3e996519862b4487101b790
Rundle Shared ThinkPad <rundleroboticsgithub@gmail.com>
2024-02-22 20:10:05 -0700

    new fixes  (NO CRASH PLS)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -27,7 +27,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.subsystems.LimitJamal;
 
 
 /**
@@ -44,7 +43,6 @@ public class RobotContainer {
   public final ArmMotor m_armmotor = new ArmMotor();
   public final JamalShooter m_shootermotor = new JamalShooter();
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final LimitJamal m_limitJamal = new LimitJamal();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   public static final CommandXboxController m_driverController =
