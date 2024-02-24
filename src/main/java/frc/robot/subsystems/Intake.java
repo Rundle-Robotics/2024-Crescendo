0489237fa91a47c83e4f0d185295d3bf7db936b5
DevenVElani <118859678+DevenVElani@users.noreply.github.com>
2024-02-23 17:27:35 -0700

    46u8q4i0jhaeijr0oo idk

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,16 +5,23 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.ArmMotor;
 
 public class Intake extends SubsystemBase {
 
   /** Creates a new Intake. */
     Spark M1;
+    boolean bottomSwitch;
+
   public Intake() {
     M1 = new Spark(2);
   }
   public void setspeed(double speed) {
-    M1.set(speed);
+    bottomSwitch = ArmMotor.getBottomSwitch();
+    if(bottomSwitch == false){
+      M1.set(speed);
+    }
+    M1.set(speed/2);
   }
   public void stop() {
     M1.set(0);
